dc84003871a9c6c208650d220ba8fd8d3fb65061
burbage stacey <rstacey06@gmail.com>
2019-10-22 20:05:50 +0000

    Automatically backed up by Learn

---
+++ lib/array_methods.rb
@@ -0,0 +1,13 @@
+def find_element_index(array, value_to_find)
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+end
+
+def find_max_value(array)
+  # Add your solution here
+end
+
+def find_min_value(array)
+  # Add your solution here
+end
